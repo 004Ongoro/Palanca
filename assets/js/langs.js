d9d22542e2388781942ccbe6f1f309a783a9a462
George Ongoro <157955004+004Ongoro@users.noreply.github.com>
2025-05-02 12:54:18 +0300

    ashfg

--- assets/js/langs.js
+++ assets/js/langs.js
@@ -7,6 +7,44 @@ const currentLang = currentPath.split("/")[1];
 
 const savedLang = localStorage.getItem("preferredLang");
 
+const translateContainer= document.querySelector('.translate-container')
+
+const languageNames = {
+  en: "English",
+  es: "Spanish",
+  pt: "Portuguese",
+  fr: "French",
+  it: "Italian",
+  hi: "Hindi",
+  ar: "Arabic",
+  ru: "Russian",
+  bn: "Bengali",
+  zh: "Chinese"
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  translateContainer.innerHTML = ""
+})
+
+const select = document.createElement("select");
+select.name = "language";
+select.id = "language";
+
+supportedLanguages.forEach(code => {
+  const option = document.createElement("option");
+  option.value = code;
+  option.textContent = languageNames[code] || code;
+  select.appendChild(option);
+});
+
+const label = document.createElement("label");
+label.setAttribute("for", "language");
+label.textContent = "Select Language: ";
+
+const container = document.getElementById("language-container");
+container.appendChild(label);
+container.appendChild(select);
+
 if (!savedLang) {
   const browserLang = navigator.language.slice(0, 2);
 
@@ -24,7 +62,7 @@ if (
   window.location.href = `/${savedLang}/`;
 }
 
-const languageSelect = document.getElementById("langs");
+const languageSelect = document.getElementById("language");
 
 if (languageSelect) {
   if (supportedLnaguages.includes(currentLang)) {
